52f2c942ebccdcd16df7f3e738144a6a29df930d
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2022-03-07 13:15:06 +0530

    updated

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.use(express.static("./frontend/build"));
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(8080, () => console.log("listening on port 8080"));
